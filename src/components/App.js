034a26685cd7c636e91cde11f97612b8a28c5e1a
Bradon Nichols <nichols.bradon@gmail.com>
2021-01-27 10:52:05 -0700

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './App.css';
 
 import Header from './Header/Header';
@@ -19,11 +19,17 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+    .then(results => {
+      this.setState({posts: results.data})
+    })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
+    .then(results => {
+      this.setState({posts: results.data})
+    })
   }
 
   deletePost() {
@@ -44,6 +50,16 @@ class App extends Component {
         <section className="App__content">
 
           <Compose />
+
+        {
+          posts.map(post => (
+            <Post key = {post.id}
+                  text = {post.text}
+                  date = {post.date}
+                  id = {post.id}
+                  updatePostFn = {this.updatePost} />
+          ))
+        }
           
         </section>
       </div>
